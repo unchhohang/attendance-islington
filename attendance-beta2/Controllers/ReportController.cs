92d4ed7531a5206c3ad63005b41f2ea04d01d044
unchhohang <unchhohang@gmail.com>
2019-04-28 23:53:28 +0545

    Role development beta

--- attendance-beta2/Controllers/ReportController.cs
+++ attendance-beta2/Controllers/ReportController.cs
@@ -27,13 +27,35 @@ namespace attendance_beta2.Controllers
         [HttpPost]
         public ActionResult DailyReport(string reportType, string GivenDate)
         {
-            
-            string sql = "SELECT A.*, S.StudentName FROM Attendances A JOIN Students S ON S.StudentId = A.StudentId where A.punchTime >= '" + GivenDate + " 00:00:000" + "' AND A.punchTime < '" + GivenDate + " 23:00:000" +"'";
-            db.List(sql);
-            var dt = db.List(sql);
-            var model = new Report().GetReport(dt);
-            return View(model);
-            
+            if (reportType == "Daily")
+            {
+
+                string sql = "SELECT A.*, S.StudentName FROM Attendances A JOIN Students S ON S.StudentId = A.StudentId where A.punchTime >= '" + GivenDate + " 00:00:000" + "' AND A.punchTime < '" + GivenDate + " 23:00:000" + "'";
+                db.List(sql);
+                var dt = db.List(sql);
+                var model = new Report().GetReport(dt);
+                return View(model);
+            }
+            else if (reportType == "Weekly")
+            {
+                string sql = "SELECT A.*, S.StudentName FROM Attendances A JOIN Students S ON S.StudentId = A.StudentId where DATEPART(week, A.punchTime) = DATEPART(week, '"+ GivenDate +"')" +
+                    "AND DATEPART(year, A.punchTime) = DATEPART(year, '" + GivenDate + "')";
+                db.List(sql);
+                var dt = db.List(sql);
+                var model = new Report().GetReport(dt);
+                return View(model);
+            }
+            else if (reportType == "Monthly")
+            {
+                string sql = "SELECT A.*, S.StudentName FROM Attendances A JOIN Students S ON S.StudentId = A.StudentId where DATEPART(month, A.punchTime) = DATEPART(month, '" + GivenDate + "')" +
+                    "AND DATEPART(year, A.punchTime) = DATEPART(year, '" + GivenDate + "')";
+                db.List(sql);
+                var dt = db.List(sql);
+                var model = new Report().GetReport(dt);
+                return View(model);
+            }
+
+            return View();
         }
 
         public ActionResult weeklyReport()
